42e46dc8dc813f84574f969deeef243ac64f3abe
alexdymov <alex.dymov@gmail.com>
2021-11-17 19:30:21 +0300

    remove one more K, fix stats with tabs behavior

--- src/hooks/game/game-stats.ts
+++ src/hooks/game/game-stats.ts
@@ -1,14 +1,16 @@
 import Vue from 'vue';
 import GameState from '../../components/game-state';
 import { debug } from '../../util/debug';
+import mutator from '../../util/mutator';
 
 export class GameStats {
     private jq: JQuery<Element>;
+    private rootOrig: JQuery<HTMLElement>;
     private root: JQuery<HTMLElement>;
     private stats: JQuery<HTMLElement>;
     private content: JQuery<HTMLElement>;
     private title: JQuery<HTMLElement>;
-    private prevState = false;
+    private allRenderedSeparately = false;
 
     constructor(public base: Vue, private state: GameState) {
         this.jq = jQuery(base.$el);
@@ -18,26 +20,50 @@ export class GameStats {
     private init() {
         require('../../style/game/game-stats.less');
 
-        this.root = jQuery('<div class="table-body-stats"><div class="TableHelper-content"/></div>').appendTo('div.table-body');
-        this.stats = this.root.find('div.TableHelper-content');
-        this.content = this.jq.find('div.TableHelper-content-stat');
+        this.root = jQuery('<div class="table-body-stats"/>').appendTo('div.table-body');
+        this.stats = jQuery('<div class="TableHelper-content"/>').appendTo(this.root);
+        this.loadElements();
         this.state.$watch('storage.flags', () => {
-            this.title = this.jq.find('div._matchtitle');
+            this.title = this.rootOrig.find('div._matchtitle');
             this.render();
+            this.observeTabSwitch();
         });
-        this.render();
         window.onresize = (e) => {
             this.render();
         };
     }
 
+    private loadElements() {
+        this.allRenderedSeparately = false;
+        this.rootOrig = this.jq.find('div.TableHelper-content > div');
+        this.content = this.rootOrig.find('div.TableHelper-content-stat');
+        this.title = this.rootOrig.find('div._matchtitle');
+        this.render();
+    }
+
+    private observeTabSwitch() {
+        mutator.mutateAdded(this.rootOrig.parent(), el => {
+            if (!el.has('div._matchtitle').length)
+                return;
+            if (this.allRenderedSeparately) {
+                this.title.remove();
+                this.content.remove();
+            }
+            this.loadElements();
+        });
+    }
+
     private render() {
-        const matches = window.matchMedia('(min-aspect-ratio: 85/50)').matches;
-        const ctr = matches ? this.stats : jQuery('div.TableHelper div.TableHelper-content > div');
-        const rerender = matches !== this.prevState;
-        matches && this.root.show() || this.root.hide();
+        const renderSeparately = this.isEnoughWidth();
+        const ctr = renderSeparately ? this.stats : this.rootOrig;
+        const rerender = renderSeparately !== this.allRenderedSeparately;
+        renderSeparately && this.root.show() || this.root.hide();
         rerender && this.content.detach().prependTo(ctr);
-        this.title && rerender && this.title.detach().prependTo(ctr);
-        this.title && (this.prevState = matches);
+        this.title.length && rerender && this.title.detach().prependTo(ctr);
+        this.title.length && (this.allRenderedSeparately = renderSeparately);
+    }
+
+    private isEnoughWidth() {
+        return window.matchMedia('(min-aspect-ratio: 85/50)').matches;
     }
 }
