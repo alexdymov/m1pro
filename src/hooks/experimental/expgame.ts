bd48a5d996d9812a9703b4c6c8649f8da69e4b4a
alexdymov <alex.dymov@gmail.com>
2022-04-10 18:17:39 +0300

    move exp

--- src/hooks/experimental/expgame.ts
+++ src/hooks/experimental/expgame.ts
@@ -1,9 +1,7 @@
 import vooker from '../../util/vue-hooker';
 import GameState from '../../components/game-state';
-import { LockableFields } from './modules/lockable-fields';
-import { TableAction } from './modules/table-action';
 
 export const expgame = process.env.NODE_ENV === 'production' ? (state: GameState) => { } : (state: GameState) => {
-    vooker.ifMount(jq => jq.is('#ui-fields'), v => { new LockableFields(state) });
-    vooker.ifMount(jq => jq.is('div.TableAction'), v => new TableAction(v, state));
+    // vooker.ifMount(jq => jq.is('#ui-fields'), v => { new LockableFields(state) });
+    // vooker.ifMount(jq => jq.is('div.TableAction'), v => new TableAction(v, state));
 }
