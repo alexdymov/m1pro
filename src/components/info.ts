077c70d47680f4c7670ab2a0a9aa9d0e294ae513
alexdymov <alex.dymov@gmail.com>
2022-06-25 18:08:32 +0300

    add donations section

--- src/components/info.ts
+++ src/components/info.ts
@@ -20,19 +20,77 @@ const opts: ComponentOptions<Vue> = {
                 <div class="Info-pro">
                     <div class="Info-pro-head">Текущая версия: ${VERSION}</div>
                     <div class="Info-pro-general">
-                        Сообщество для обсуждения: 
-                        <a href="https://discord.gg/YFzfEBcCKA" rel="nofollow" class="btn">
-                            <img src="https://camo.githubusercontent.com/f57032cdfa9884f57d69ede973b7c876a17afe1519b0aa271e06299651afbf81/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f446973636f72642d3732383944413f7374796c653d666c6174266c6f676f3d646973636f7264266c6f676f436f6c6f723d7768697465" alt="Discord" data-canonical-src="https://img.shields.io/badge/Discord-7289DA?style=flat&amp;logo=discord&amp;logoColor=white" style="max-width: 100%;">
-                        </a>
-                        <a target="_blank" rel="noopener noreferrer" class="btn" href="https://camo.githubusercontent.com/3f29b62bfbf1a9947a11107ca4aa6296fb2b35acde2a1896019f60194a1292d4/68747470733a2f2f696d672e736869656c64732e696f2f646973636f72642f393637343131323132333639343136313932">
-                            <img src="https://camo.githubusercontent.com/3f29b62bfbf1a9947a11107ca4aa6296fb2b35acde2a1896019f60194a1292d4/68747470733a2f2f696d672e736869656c64732e696f2f646973636f72642f393637343131323132333639343136313932" alt="Online" data-canonical-src="https://img.shields.io/discord/967411212369416192" style="max-width: 100%;">
-                        </a>
+                            Сообщество для обсуждения: 
+                            <a href="https://discord.gg/YFzfEBcCKA" rel="nofollow" class="btn">
+                                <img src="https://camo.githubusercontent.com/f57032cdfa9884f57d69ede973b7c876a17afe1519b0aa271e06299651afbf81/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f446973636f72642d3732383944413f7374796c653d666c6174266c6f676f3d646973636f7264266c6f676f436f6c6f723d7768697465" alt="Discord" data-canonical-src="https://img.shields.io/badge/Discord-7289DA?style=flat&amp;logo=discord&amp;logoColor=white" style="max-width: 100%;">
+                            </a>
+                            <a target="_blank" rel="noopener noreferrer" class="btn" href="https://camo.githubusercontent.com/3f29b62bfbf1a9947a11107ca4aa6296fb2b35acde2a1896019f60194a1292d4/68747470733a2f2f696d672e736869656c64732e696f2f646973636f72642f393637343131323132333639343136313932">
+                                <img src="https://camo.githubusercontent.com/3f29b62bfbf1a9947a11107ca4aa6296fb2b35acde2a1896019f60194a1292d4/68747470733a2f2f696d672e736869656c64732e696f2f646973636f72642f393637343131323132333639343136313932" alt="Online" data-canonical-src="https://img.shields.io/discord/967411212369416192" style="max-width: 100%;">
+                            </a>
+                    </div>
+                    <div class="Info-pro-donations">
+                        <h2>Пожертвования</h2>
+                        <div>
+                            <div class="form form-horizontal form-small">
+                                <div class="title title-5">Paypal</div>
+                                <div>
+                                    <form action="https://www.paypal.com/donate" method="post" target="_blank">
+                                        <input type="hidden" name="hosted_button_id" value="DQBQJ5UQEHD3W" />
+                                        <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
+                                        <img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" />
+                                    </form>
+                                </div>
+                            </div>
+                            <div class="form form-horizontal form-small">
+                                <div class="title title-5">YooMoney</div>
+                                <form method="POST" action="https://yoomoney.ru/quickpay/confirm.xml" target="_blank">
+                                    <input type="hidden" name="receiver" value="410015004852328">
+                                    <input type="hidden" name="quickpay-form" value="donate">
+                                    <input type="hidden" name="targets" value="m1pro">
+                                    <div class="form2-row yoo-controls">
+                                        <input type="text" name="formcomment" placeholder="Комментарий">
+                                        <input type="number" name="sum" value="500" data-type="number" max="15000">
+                                        <label>
+                                            <button type="button" class="button button-small" v-on:click='paymentType = "PC"' :class='{ _selected: paymentType === "PC" }'>
+                                                <span class="yoo-icon yoo-icon-purse"></span>
+                                            </button>
+                                            <input type="radio" name="paymentType" value="PC" :checked='paymentType === "PC"'>
+                                        </label>
+                                        <label>
+                                            <button type="button" class="button button-small" v-on:click='paymentType = "AC"' :class='{ _selected: paymentType === "AC" }'>
+                                                <span class="yoo-icon yoo-icon-card"></span>
+                                            </button>
+                                            <input type="radio" name="paymentType" value="AC" :checked='paymentType === "AC"'>
+                                        </label>
+                                    </div>
+                                    <div class="form2-row buttons">
+                                        <input type="submit" class="button button-small" value="Donate">
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
                     </div>
                     <div class="Info-pro-history">${hist}</div>
                 </div>
             </div>
         </div>
     </div>`,
+    data: function() {
+        return {
+            paymentType: 'PC'
+        };
+    },
+    // computed: {
+    //     paymentType: function () {
+    //         debug('paymentType', jQuery('div.yoo-controls input[name="paymentType"]:checked').val())
+    //         return jQuery('div.yoo-controls input[name="paymentType"]:checked').val() ?? "PC";
+    //     }
+    // },
+    methods: {
+        setPT: function (val) {
+            $(`input[name=paymentType][value="${val}"]`).prop('checked', true);
+        }
+    },
     mounted() {
         const jq = jQuery(this.$el);
         const main = jq.find('div.Info-main');
@@ -67,7 +125,7 @@ const opts: ComponentOptions<Vue> = {
         });
         jq.find('div.Info-pro-history li strong').each((i, el) => {
             jQuery(el).replaceWith(() => `<span class="info-icon ${el.textContent}"/>`);
-        })
+        });
     }
 };
 
