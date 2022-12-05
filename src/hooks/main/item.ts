8fef895f0014aaba6da1107add678c204456b28b
alexdymov <alex.dymov@gmail.com>
2022-12-05 16:43:14 +0300

    add item price loading state, handle errors, hide price marker if price unavailable

--- src/hooks/main/item.ts
+++ src/hooks/main/item.ts
@@ -1,7 +1,6 @@
 import Vue from 'vue';
 import MainState from '../../components/main-state';
 import { ItemData, ItemData2 } from '../../shared/beans';
-import { debug } from '../../util/debug';
 
 declare module 'vue/types/vue' {
     interface Vue {
@@ -22,13 +21,22 @@ export class Item {
 
         // debug(JSON.parse(JSON.stringify(item)));
 
-        const mark = jQuery('<div class="_flex _mark ion-ios-cart" />');
+        const mark = jQuery('<div class="_flex _mark ion-ios-cart"><span>...</span></div>');
         this.jq.find('div._left').find('div._mark.ion-ios-cart').remove().end().append(mark);
         this.base.$watch('item', it => {
-            this.state.getItemPrice(it.item_proto_id ?? it.thing_prototype_id).then(price => mark.children().remove().end().append(`<span>${price}</span>`));
+            const id = it.item_proto_id ?? it.thing_prototype_id;
+            this.state.getItemPrice(id)
+                .then(price => {
+                    if (price === null) {
+                        mark.hide();
+                    } else {
+                        mark.css('display', 'flex').children().remove().end().append(`<span>${price}</span>`);
+                    }
+                })
+                .fail(e => mark.css('color', '#da4553'));
             if (item.user_id === window.API.user.user_id) {
                 this.state.lots.things.some(lot => {
-                    if (lot.thing_prototype_id === (item.item_proto_id ?? item.thing_prototype_id)) {
+                    if (lot.thing_prototype_id === id) {
                         mark.css('color', 'green');
                         return true;
                     }
